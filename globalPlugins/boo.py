f0b6aa49dba121190ed6b8f87728492cddb5190e
Ty <60036684+TyGillespie@users.noreply.github.com>
2021-02-17 22:49:45 -0700

    Formatting updates.

--- globalPlugins/boo.py
+++ globalPlugins/boo.py
@@ -7,51 +7,51 @@ import nvwave
 from random import choice, randint
 
 def playSpookyRecording():
-    CallLater(randint(1000,100000), playSpookyRecording)
-    nvwave.playWaveFile(os.path.dirname(__file__)+"\\boo\\"+getRandomFile())
+	CallLater(randint(1000,100000), playSpookyRecording)
+	nvwave.playWaveFile(os.path.dirname(__file__)+"\\boo\\"+getRandomFile())
 
-#Trigger random confusing errors.
+# Trigger random confusing errors.
 def recursiveStackBuilder1():
-    if randint(1,200) == 30:
-        log.exception("hahahahahahaha!", stack_info = True)
-        return
-    if randint(0,1):
-        recursiveStackBuilder1()
-    else:
-        recursiveStackBuilder2()
+	if randint(1, 200) == 30:
+		log.exception("hahahahahahaha!", stack_info = True)
+		return
+	if randint(0, 1):
+		recursiveStackBuilder1()
+	else:
+		recursiveStackBuilder2()
 
 def recursiveStackBuilder2():
-    if randint(1,100) == 500:
-        log.exception("boo!", stack_info = True)
-        return
-    if randint(0,1):
-        recursiveStackBuilder1()
-    else:
-        recursiveStackBuilder2()
+	if randint(1,100) == 500:
+		log.exception("boo!", stack_info = True)
+		return
+	if randint(0,1):
+		recursiveStackBuilder1()
+	else:
+		recursiveStackBuilder2()
 
 class Spooker(object):
-    def event_gainFocus(self):
-        if not randint(0,100):
-            recursiveStackBuilder1()
-        try:
-            super(Spooker, self).event_gainFocus()
-        except AttributeError:
-            pass
+	def event_gainFocus(self):
+		if not randint(0,100):
+			recursiveStackBuilder1()
+		try:
+			super(Spooker, self).event_gainFocus()
+		except AttributeError:
+			pass
+
 def getRandomFile():
-    return choice(os.listdir(os.path.dirname(__file__)+"\\boo"))
+	return choice(os.listdir(os.path.dirname(__file__) + "\\boo"))
 
 class GlobalPlugin(globalPluginHandler.GlobalPlugin):
-    def chooseNVDAObjectOverlayClasses(self, obj, clsList):
-        clsList.insert(0, Spooker)
+	def chooseNVDAObjectOverlayClasses(self, obj, clsList):
+		clsList.insert(0, Spooker)
 
-    def executeGesture(self, gesture):
-        if randint(0,500) == 0:
-            recursiveStackBuilder2()
-        return inputCore.manager.oldExecuteGesture(gesture)
+	def executeGesture(self, gesture):
+		if randint(0,500) == 0:
+			recursiveStackBuilder2()
+		return inputCore.manager.oldExecuteGesture(gesture)
 
-    def __init__(self):
-        super(GlobalPlugin, self).__init__()
-        CallLater(randint(1000,100000), playSpookyRecording)
-        inputCore.manager.oldExecuteGesture = inputCore.manager.executeGesture
-        inputCore.manager.executeGesture = self.executeGesture
-        
+	def __init__(self):
+		super(GlobalPlugin, self).__init__()
+		CallLater(randint(1000,100000), playSpookyRecording)
+		inputCore.manager.oldExecuteGesture = inputCore.manager.executeGesture
+		inputCore.manager.executeGesture = self.executeGesture
